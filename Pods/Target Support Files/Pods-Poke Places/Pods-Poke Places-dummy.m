1b4741ff7e7cc9767ea40a8df1cd564376f22b2d
Ryan Ball <ryanball@Ryans-iMac.local>
2016-07-27 22:07:11 +0100

    Centre on user location button added along with pokestop and gym buttons.

---
+++ Pods/Target Support Files/Pods-Poke Places/Pods-Poke Places-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Poke_Places : NSObject
+@end
+@implementation PodsDummy_Pods_Poke_Places
+@end
---
+++ Poke Places/PokePlaces-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokePlaces-Bridging-Header.h
+//  Poke Places
+//
+//  Created by Ryan Ball on 27/07/2016.
+//  Copyright © 2016 Ryan Ball. All rights reserved.
+//
+
+#import "GeoFire.h"
+
